983d34b30c6f76506deeeea9c494a8868bfcb5e9
Junk-debug <maks.naumovich@gmail.com>
2024-07-07 23:20:04 +0300

    fix: delete eslint rule disabling comment

--- src/components/errorBoundary.tsx
+++ src/components/errorBoundary.tsx
@@ -27,7 +27,6 @@ class ErrorBoundary extends Component<Props, State> {
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    // eslint-disable-next-line no-console
     console.error(error, errorInfo);
   }
 
