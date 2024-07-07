20d5d922486f224ce84278296887a7903ad0a645
Junk-debug <maks.naumovich@gmail.com>
2024-07-07 16:11:35 +0300

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ class App extends PureComponent {
   render() {
     return (
       <main className="min-h-screen bg-gray-100 text-zinc-800">
+        <h1 className="text-6xl font-bold text-center pt-8">
+          Rick and Morty characters
+        </h1>
         <ErrorBoundary>
           <Search />
         </ErrorBoundary>
