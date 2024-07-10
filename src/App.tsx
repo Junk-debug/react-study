6ba9356ea7cb95d78817292e6ef1caf295fcb9e7
Junk-debug <maks.naumovich@gmail.com>
2024-07-10 11:32:25 +0200

    fix: rewrite App to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { PureComponent } from "react";
 import ErrorBoundary from "./components/errorBoundary";
 import CharactersPage from "./pages/charactersPage/charactersPage";
 
-class App extends PureComponent {
-  render() {
-    return (
-      <main className="min-h-screen bg-gray-100 text-zinc-800">
-        <ErrorBoundary>
-          <CharactersPage />
-        </ErrorBoundary>
-      </main>
-    );
-  }
+function App() {
+  return (
+    <main className="min-h-screen bg-gray-100 text-zinc-800">
+      <ErrorBoundary>
+        <CharactersPage />
+      </ErrorBoundary>
+    </main>
+  );
 }
 
 export default App;
