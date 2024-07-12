6a0a74037e615028cea213511f1fcfe78b8f6c24
Junk-debug <maks.naumovich@gmail.com>
2024-07-13 00:55:36 +0200

    feat: make currentPage to be url search param

--- src/pages/charactersPage/useSearchLogic.ts
+++ src/pages/charactersPage/useSearchLogic.ts
@@ -1,20 +1,26 @@
 import { AxiosError } from "axios";
 import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import api, { CharactersResponse, ApiError } from "../../api/api";
 import useLocalStorage from "../../hooks/useLocalStorage";
 
 export default function useSearchLogic() {
   const [searchInputValue, setSearchInputValue] = useLocalStorage("search", "");
-  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = searchParams.get("page")
+    ? Number(searchParams.get("page"))
+    : 1;
+
   const [apiResponse, setApiResponse] = useState<CharactersResponse | null>(
     null,
   );
   const [error, setError] = useState<AxiosError<ApiError> | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [testError, setTestError] = useState<boolean>(false);
 
   const handleRequest = (name: string, page: number) => {
     setIsLoading(true);
+    setError(null);
 
     api
       .getCharacters({ name: name.trim(), page })
@@ -30,12 +36,12 @@ export default function useSearchLogic() {
   };
 
   const handleSearchButtonClick = () => {
-    setCurrentPage(1);
+    setSearchParams({ page: "1" });
     handleRequest(searchInputValue, 1);
   };
 
   const handlePageButtonClick = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
+    setSearchParams({ page: pageNumber.toString() });
     handleRequest(searchInputValue, pageNumber);
   };
 
@@ -61,7 +67,5 @@ export default function useSearchLogic() {
     isLoading,
     error,
     handleErrorReset,
-    testError,
-    handleErrorButtonClick: () => setTestError(true),
   };
 }
