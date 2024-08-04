f357f321678f7bce841a84b4958176c7fb28504a
Junk-debug <maks.naumovich@gmail.com>
2024-08-04 17:56:33 +0200

    add title

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import { useThemeContext } from "../context/themeContext";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,6 +15,9 @@ export default function RootLayout({
       data-theme={currentTheme}
       className={`transition-colors min-h-screen bg-white text-zinc-800 dark:bg-zinc-950 dark:text-zinc-50 ${inter.className}`}
     >
+      <Head>
+        <title>Rick and Morty app</title>
+      </Head>
       {children}
     </main>
   );
